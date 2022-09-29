d62d47636a91cbcd1dbd042abc2c5d52e2307b8c
Eddy-hub19 <75374150+Eddy-hub19@users.noreply.github.com>
2022-09-29 18:24:47 +0000

    Deploying to gh-pages from @ Eddy-hub19/goit-js-hw-09@98b79e994b6981b46bcee3c11319564a65065a1b 🚀

---
+++ 01-color-switcher.5fa7d58f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");console.log(t),t.addEventListener("click",(function(t){t.target.classList.toggle("green"),window.document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))})),e.addEventListener("click",(function(t){t.target.classList.toggle("red"),window.document.body.style.background=""}))}();
+//# sourceMappingURL=01-color-switcher.5fa7d58f.js.map
---
+++ 01-color-switcher.f547a03c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");console.log(t),t.addEventListener("click",(function(t){t.target.classList.toggle("green"),window.document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`})),e.addEventListener("click",(function(t){t.target.classList.toggle("red"),window.document.body.style.background=""}));
+//# sourceMappingURL=01-color-switcher.f547a03c.js.map
